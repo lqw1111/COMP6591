c57e33226fd57e811d051de4dcb99f70a657809c
lqw1111 <369487836luo@gmail.com>
2019-11-18 17:01:53 -0500

    add file path

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javax.xml.crypto.Data;
 
 public class Main {
     public static void main(String[] args) {
-        DataBase dataBase = new DataBase("");
+        DataBase dataBase = new DataBase("/Users/luoqinwei/Documents/COMP6591_Project/database");
         ProvenanceSemiring provenanceSemiring = new ProvenanceSemiring(5, "", dataBase);
         provenanceSemiring.calculate();
     }
