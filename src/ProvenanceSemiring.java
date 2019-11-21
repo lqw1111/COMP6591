24f82e1614ecfa3b632ec9a1b134b938339de666
dianlutu <jeffrey95@163.com>
2019-11-20 22:53:12 -0500

    finish union method

--- src/ProvenanceSemiring.java
+++ src/ProvenanceSemiring.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ProvenanceSemiring {
 
@@ -45,8 +46,44 @@ public class ProvenanceSemiring {
         return projectTable;
     }
 
-    public void union(){
+    public Table union(Table tableA, Table tableB)throws Exception{
 
+        if(tableA.title.size()!=tableB.title.size()){
+            throw new Exception("if two table union they must have same number of column");
+        }
+
+        for (String columnA:
+             tableA.title) {
+            if(!tableB.title.contains(columnA)){
+                throw new Exception("wrong union");
+            }
+        }
+
+        Table unionTable = new Table("unionTable");
+
+        ArrayList<ArrayList<String>> newTableOfTableAB = new ArrayList<ArrayList<String>>();
+        newTableOfTableAB = tableA.content;
+
+        HashMap<Integer,Integer> orderOfAMapToOrderOfB = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < tableA.title.size(); i++) {
+            orderOfAMapToOrderOfB.put(i,tableB.title.indexOf(tableA.title.get(i)));
+        }
+
+        for (ArrayList<String> lineInTableB:
+            tableB.content) {
+            ArrayList<String> newLine = new ArrayList<>();
+            for (int i = 0; i < tableB.title.size(); i++) {
+                newLine.add(lineInTableB.get(orderOfAMapToOrderOfB.get(i)));
+            }
+            newTableOfTableAB.add(newLine);
+        }
+
+        unionTable.column = tableA.column;
+        unionTable.title = tableA.title;
+        unionTable.content = newTableOfTableAB;
+
+        return unionTable;
     }
 
     public void select(String conditions,Table table){
